64910806095ecb140c23488d1a3bc218e869bd9a
Soraya Sanchez <sorayasanchez0503@gmail.com>
2023-03-26 10:32:40 -0500

    configurar prettier y eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'standard-with-typescript'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json',
+  },
+  plugins: ['react'],
+  rules: {
+    '@typescript-eslint/consistent-type-assertions': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    quotes: 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-uses-react': 0,
+    'react/jsx-indent': ['error', 2],
+    'react/jsx-curly-newline': [
+      'error',
+      {
+        multiline: 'consistent',
+        singleline: 'consistent',
+      },
+    ],
+    'comma-dangle': 'off',
+    'react/jsx-tag-spacing': [
+      'error',
+      {
+        closingSlash: 'never',
+        beforeSelfClosing: 'always',
+        afterOpening: 'never',
+        beforeClosing: 'never',
+      },
+    ],
+
+    // '@typescript-eslint/semi': 'off'    --- ; al final
+  },
+}
