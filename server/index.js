01b199035c7cc8dd2d976fdaf6dd84846c89299b
Gasnis <gastonsaravia112@gmail.com>
2023-03-27 11:43:59 +0200

    Backend

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const morgan = require("morgan")
+const app = express()
+const routes = require("./routes")
+const cors = require("cors")
+require("dotenv").config()
+require("./config/db")
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(morgan("dev"))
+app.use(
+    cors({
+      origin: "http://localhost:3001",
+      methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+      credentials: true,
+    })
+  );
+
+app.use("./api", routes)
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server listen on ${process.env.PORT}`);
+  });
