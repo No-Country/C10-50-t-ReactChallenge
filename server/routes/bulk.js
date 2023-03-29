2e0498652dca550113c88ba85bd82dc834dfcf2d
Gasnis <gastonsaravia112@gmail.com>
2023-03-30 00:45:38 +0200

    delete bulk

--- server/routes/bulk.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const bulkController = require("../controllers/product.controller");
-
-router.post("/", bulkController.bulk);
-
-module.exports = router;
