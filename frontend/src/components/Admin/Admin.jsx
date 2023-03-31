af7c8423ff0df8797aded66604c42313570ef988
Zireuz <willdevmor@gmail.com>
2023-03-30 20:49:19 -0500

    Nuevo Commit

--- frontend/src/components/Admin/Admin.jsx
+++ frontend/src/components/Admin/Admin.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import CrudAdm from './CrudAdm'
 
 const Admin = () => {
-  return <div>Admin</div>
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <br />
+      <CrudAdm />
+      <br />
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+    </div>
+  )
 }
 
 export default Admin
---
+++ frontend/src/components/Admin/CrudAdm.jsx
@@ -0,0 +1,10 @@
+const CrudAdm = () => {
+  return (
+    <div>
+      <h2>CrudAdm</h2>
+      <p>Item 1</p>
+    </div>
+  )
+}
+
+export default CrudAdm
