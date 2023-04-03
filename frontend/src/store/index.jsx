c409464143ac11933a713e963034f9f78ae48d28
noehernandez24 <evilld94@gmail.com>
2023-04-03 17:48:37 -0600

    ADD: global state ticket

--- frontend/src/store/index.jsx
+++ frontend/src/store/index.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import productsSlice from './slices/products.slice'
+import ticketSlice from './slices/tickets.slice'
 
 export default configureStore({
   reducer: {
     getProducts: productsSlice,
+    tickets: ticketSlice,
   },
 })
