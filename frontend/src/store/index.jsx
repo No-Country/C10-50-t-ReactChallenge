805989e61cfa13f05864ddf4a496d5722a4d85b6
Gasnis <gastonsaravia112@gmail.com>
2023-04-04 03:20:31 +0200

    cositas que pasaan

--- frontend/src/store/index.jsx
+++ frontend/src/store/index.jsx
@@ -1,11 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import productsSlice from './slices/products.slice'
 import ticketSlice from './slices/tickets.slice'
+import staffSlice from './slices/staff.slice'
 
 export default configureStore({
   reducer: {
     products: productsSlice,
     tickets: ticketSlice,
-
+    staff: staffSlice,
   },
 })
