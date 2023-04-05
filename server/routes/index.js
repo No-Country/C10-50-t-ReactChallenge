969e1ef03ce28907d8e084710a2c8d54cfbe71a6
Julio Dalmau <104108635+JulioDalmau@users.noreply.github.com>
2023-04-05 00:03:56 -0300

    modified

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,21 +5,11 @@ const staff = require("./staff");
 const ticket = require("./ticket");
 const bulk = require("./bulk");
 const auth = require("./auth");
-<<<<<<< HEAD
-const admin = require("./admin");
-const prueba = require("./prueba")
-=======
->>>>>>> 83f2b168b6e797af216ded470c1217a3f6df5308
 
 router.use("/menu", menu);
 router.use("/staff", staff);
 router.use("/ticket", ticket);
 router.use("/bulk", bulk);
 router.use("/auth", auth);
-<<<<<<< HEAD
-router.use("/admin", admin);
-router.use("/prueba", prueba)
-=======
->>>>>>> 83f2b168b6e797af216ded470c1217a3f6df5308
 
 module.exports = router;
