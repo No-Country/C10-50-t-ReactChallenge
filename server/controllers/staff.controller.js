5f8da721a5bc58ccc5b9944f00ef2bce222ff285
Julio Dalmau <104108635+JulioDalmau@users.noreply.github.com>
2023-04-05 14:02:22 -0300

    Update staff.controller.js

--- server/controllers/staff.controller.js
+++ server/controllers/staff.controller.js
@@ -1,6 +1,7 @@
 const StaffService = require("../services/staff.services");
 
 class StaffController {
+
   static async getStaff(req, res) {
     const { data, error } = await StaffService.getAllStaff();
     if (error) {
@@ -8,14 +9,6 @@ class StaffController {
     }
     res.status(200).send(data);
   }
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-  
-  
- 
-=======
->>>>>>> 2334b327f044ed8cae5e2e3c00dbafb7951ba805
 
   static async createStaff(req, res) {
     const { data, error } = await StaffService.createStaff(req.body);
@@ -35,21 +28,13 @@ class StaffController {
 
   static async deleteStaff(req, res) {
     const { id } = req.params;
-<<<<<<< HEAD
     const { data, error } = await StaffService.deleteStaff(id);
-=======
-    console.log(id);
-    const { data, error } = await StaffService.deleteUser(id);
->>>>>>> 2334b327f044ed8cae5e2e3c00dbafb7951ba805
     if (error) {
       return res.status(404).send(error._message);
     }
     res.send(data);
   }
-<<<<<<< HEAD
-=======
->>>>>>> 83f2b168b6e797af216ded470c1217a3f6df5308
->>>>>>> 2334b327f044ed8cae5e2e3c00dbafb7951ba805
+  
 }
 
 module.exports = StaffController;
