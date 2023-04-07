72cdbd0e81b1336ff2a20b4ebefd34ba4a32d160
Julio Dalmau <104108635+JulioDalmau@users.noreply.github.com>
2023-04-07 15:43:34 -0300

    Add tickets

--- server/utils/tickets.js
+++ server/utils/tickets.js
@@ -1,4 +1,4 @@
-const productsToBulk = require("./products")
+const productsToBulk = require("./products");
 
 const ticketsToBulk = [
   {
@@ -21,7 +21,26 @@ const ticketsToBulk = [
     status: "Delivered",
     date: "2023-03-30T18:01:22.785Z",
   },
+  {
+    clientName: "Clent",
+    staff: "Emily",
+    table: "08",
+    totalPrice: 19.97,
+    paymentMethod: "cash",
+    order: ["Taco", "Beer"],
+    status: "Delivered",
+    date: "2023-03-30T18:01:22.785Z",
+  },
+  {
+    clientName: "Maria",
+    staff: "John",
+    table: "01",
+    totalPrice: 19.97,
+    paymentMethod: "cash",
+    order: ["Fish and Chips", "Beer"],
+    status: "Delivered",
+    date: "2023-03-30T18:01:22.785Z",
+  },
 ];
-   
-module.exports = ticketsToBulk
 
+module.exports = ticketsToBulk;
