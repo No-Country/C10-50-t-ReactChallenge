8f092e029f4a6f2d4181c38b31d2cec37c4a1a0c
Julio Dalmau <104108635+JulioDalmau@users.noreply.github.com>
2023-04-18 12:24:29 -0300

    Add form

--- server/utils/products.js
+++ server/utils/products.js
@@ -1,154 +1,168 @@
 const productsToBulk = [
-    {
-        "name": "Pizza",
-        "image": "https://i.imgur.com/jZTulSY.jpg",
-        "category": "MainDishes",
-        "price": 12.02,
-        "time": 37,
-        "available": true,
-        "description": "A classic Italian dish made with tomato sauce, cheese, and various toppings on a thin crust.",
-        "date": "2022-04-30T18:01:22.785Z"
-      },
-      {
-        "name": "Hamburger",
-        "image": "https://i.imgur.com/4J5fl9H.jpg",
-        "category": "MainDishes",
-        "price": 8.99,
-        "time": 15,
-        "available": true,
-        "description": "A sandwich consisting of one or more cooked patties of ground meat, usually beef, placed inside a sliced bread roll or bun.",
-        "date": "2022-04-30T18:01:22.785Z"
-      },
-      {
-        "name": "Coca Cola",
-        "image": "https://i.imgur.com/rZjfto8.jpg",
-        "category": "Drinks",
-        "price": 1.50,
-        "time": 5,
-        "available": false,
-        "description": "A carbonated soft drink manufactured by The Coca-Cola Company.",
-        "date": "2022-04-30T18:01:22.785Z"
-      },
-      {
-        "name": "Sushi",
-        "image": "https://i.imgur.com/1nNTcS6.jpg",
-        "category": "MainDishes",
-        "price": 24.99,
-        "time": 30,
-        "available": true,
-        "description": "A Japanese dish consisting of small balls or rolls of vinegar-flavored cold cooked rice served with a garnish of raw fish, vegetables, or egg.",
-        "date": "2022-04-30T18:01:22.785Z"
-      },
-      {
-        "name": "Taco",
-        "image": "https://i.imgur.com/9YXUygN.jpg",
-        "category": "Appetizers",
-        "price": 6.99,
-        "time": 10,
-        "available": true,
-        "description": "A traditional Mexican dish consisting of a corn or wheat tortilla filled with various ingredients, such as beef, chicken, beans, cheese, lettuce, and tomatoes.",
-        "date": "2022-04-30T18:01:22.785Z"
-      },
-      {
-        "name": "Beer",
-        "image": "https://i.imgur.com/l5A8RhL.jpg",
-        "category": "Drinks",
-        "price": 4.99,
-        "time": 5,
-        "available": true,
-        "description": "An alcoholic drink made from malted barley, hops, and water.",
-        "date": "2022-04-30T18:01:22.785Z"
-      },
-      {
-        "name": "Pasta",
-        "image": "https://i.imgur.com/5Z2VUS4.jpg",
-        "category": "MainDishes",
-        "price": 12.99,
-        "time": 20,
-        "available": true,
-        "description": "An Italian dish made with noodles or other shapes, usually served with a sauce, meat, and vegetables.",
-        "date": "2022-04-30T18:01:22.785Z"
-      },
-      {
-        "name": "Ice Cream",
-        "image": "https://i.imgur.com/1bcfyVL.jpg",
-        "category": "Desserts",
-        "price": 5.99,
-        "time": 5,
-        "available": true,
-        "description": "A frozen dessert made from cream or milk, sugar, and various flavorings.",
-        "date": "2022-04-30T18:01:22.785Z"
-      },
-      {
-        "name": "Burrito",
-        "image": "https://i.imgur.com/ZN1rkW1.jpg",
-        "category": "Appetizers",
-        "price": 9.99,
-        "time": 15,
-        "available": true,
-        "description": "A Mexican dish consisting of a tortilla filled with meat, beans, cheese, and other ingredients.",
-        "date": "2022-04-30T18:01:22.785Z"
-      },
-      {
-        "name": "Fried Rice",
-        "image": "https://i.imgur.com/TEapU8u.jpg",
-        "category": "MainDishes",
-        "price": 11.99,
-        "time": 20,
-        "available": true,
-        "description": "A Chinese dish made with rice that has been cooked and stir-fried in a wok or a frying pan and usually mixed with other ingredients such as eggs, vegetables, and meat.",
-        "date": "2022-04-30T18:01:22.785Z"
-      },
-      {
-        "name": "Pho",
-        "image": "https://i.imgur.com/QKfv5TT.jpg",
-        "category": "MainDishes",
-        "price": 14.99,
-        "time": 30,
-        "available": true,
-        "description": "A Vietnamese soup consisting of broth, rice noodles, herbs, and meat (usually beef) and garnished with lime, bean sprouts, and other accompaniments.",
-        "date": "2022-04-30T18:01:22.785Z"
-      },
-      {
-        "name": "Margarita",
-        "image": "https://i.imgur.com/PKBBeQL.jpg",
-        "category": "Drinks",
-        "price": 7.99,
-        "time": 5,
-        "available": true,
-        "description": "A cocktail consisting of tequila, lime juice, and orange liqueur, often served with salt on the rim of the glass.",
-        "date": "2022-04-30T18:01:22.785Z"
-      },
-      {
-        "name": "Fish and Chips",
-        "image": "https://i.imgur.com/XX60O7K.jpg",
-        "category": "MainDishes",
-        "price": 15.99,
-        "time": 25,
-        "available": true,
-        "description": "A British dish consisting of battered fish and deep-fried potatoes, often served with vinegar and tartar sauce.",
-        "date": "2022-04-30T18:01:22.785Z"
-      },
-      {
-        "name": "Pad Thai",
-        "image": "https://i.imgur.com/2wJHox1.jpg",
-        "category": "MainDishes",
-        "price": 13.99,
-        "time": 25,
-        "available": true,
-        "description": "A Thai dish made with stir-fried rice noodles, tofu, shrimp, egg, and bean sprouts, flavored with tamarind, fish sauce, and palm sugar.",
-        "date": "2022-04-30T18:01:22.785Z"
-      },
-      {
-        "name": "Hot Dog",
-        "image": "https://i.imgur.com/b93p0Tb.jpg",
-        "category": "Appetizers",
-        "price": 5.99,
-        "time": 10,
-        "available": true,
-        "description": "A type of sausage served in a sliced bun and typically garnished with ketchup, mustard, and onions.",
-        "date": "2022-04-30T18:01:22.785Z"
-      } 
-]
+  {
+    name: "Pizza",
+    image: "https://i.imgur.com/jZTulSY.jpg",
+    category: "MainDishes",
+    price: 12.02,
+    time: 37,
+    available: true,
+    description:
+      "A classic Italian dish made with tomato sauce, cheese, and various toppings on a thin crust.",
+    date: "2022-04-30T18:01:22.785Z",
+  },
+  {
+    name: "Hamburger",
+    image: "https://i.imgur.com/4J5fl9H.jpg",
+    category: "MainDishes",
+    price: 8.99,
+    time: 15,
+    available: true,
+    description:
+      "A sandwich consisting of one or more cooked patties of ground meat, usually beef, placed inside a sliced bread roll or bun.",
+    date: "2022-04-30T18:01:22.785Z",
+  },
+  {
+    name: "Coca Cola",
+    image: "https://i.imgur.com/rZjfto8.jpg",
+    category: "Drinks",
+    price: 1.5,
+    time: 5,
+    available: false,
+    description:
+      "A carbonated soft drink manufactured by The Coca-Cola Company.",
+    date: "2022-04-30T18:01:22.785Z",
+  },
+  {
+    name: "Sushi",
+    image: "https://i.imgur.com/1nNTcS6.jpg",
+    category: "MainDishes",
+    price: 24.99,
+    time: 30,
+    available: true,
+    description:
+      "Corn or wheat tortilla filled with various ingredients, such as beef, chicken, beans, cheese, lettuce, and tomatoes.",
+    date: "2022-04-30T18:01:22.785Z",
+  },
+  {
+    name: "Taco",
+    image: "https://i.imgur.com/9YXUygN.jpg",
+    category: "Appetizers",
+    price: 6.99,
+    time: 10,
+    available: true,
+    description:
+      "Corn or wheat tortilla filled with various ingredients, such as beef, chicken, beans, cheese, lettuce, and tomatoes.",
+    date: "2022-04-30T18:01:22.785Z",
+  },
+  {
+    name: "Beer",
+    image: "https://i.imgur.com/l5A8RhL.jpg",
+    category: "Drinks",
+    price: 4.99,
+    time: 5,
+    available: true,
+    description: "An alcoholic drink made from malted barley, hops, and water.",
+    date: "2022-04-30T18:01:22.785Z",
+  },
+  {
+    name: "Pasta",
+    image: "https://i.imgur.com/5Z2VUS4.jpg",
+    category: "MainDishes",
+    price: 12.99,
+    time: 20,
+    available: true,
+    description:
+      "An Italian dish made with noodles or other shapes, usually served with a sauce, meat, and vegetables.",
+    date: "2022-04-30T18:01:22.785Z",
+  },
+  {
+    name: "Ice Cream",
+    image: "https://i.imgur.com/1bcfyVL.jpg",
+    category: "Desserts",
+    price: 5.99,
+    time: 5,
+    available: true,
+    description:
+      "A frozen dessert made from cream or milk, sugar, and various flavorings.",
+    date: "2022-04-30T18:01:22.785Z",
+  },
+  {
+    name: "Burrito",
+    image: "https://i.imgur.com/ZN1rkW1.jpg",
+    category: "Appetizers",
+    price: 9.99,
+    time: 15,
+    available: true,
+    description:
+      "A Mexican dish consisting of a tortilla filled with meat, beans, cheese, and other ingredients.",
+    date: "2022-04-30T18:01:22.785Z",
+  },
+  {
+    name: "Fried Rice",
+    image: "https://i.imgur.com/TEapU8u.jpg",
+    category: "MainDishes",
+    price: 11.99,
+    time: 20,
+    available: true,
+    description:
+      "Rice that has been cooked and stir-fried in a wok or a frying pan, eggs, vegetables, and meat.",
+    date: "2022-04-30T18:01:22.785Z",
+  },
+  {
+    name: "Pho",
+    image: "https://i.imgur.com/QKfv5TT.jpg",
+    category: "MainDishes",
+    price: 14.99,
+    time: 30,
+    available: true,
+    description:
+      "Rice noodles, herbs, and meat (usually beef) and garnished with lime, bean sprouts, and other accompaniments.",
+    date: "2022-04-30T18:01:22.785Z",
+  },
+  {
+    name: "Margarita",
+    image: "https://i.imgur.com/PKBBeQL.jpg",
+    category: "Drinks",
+    price: 7.99,
+    time: 5,
+    available: true,
+    description:
+      "A cocktail consisting of tequila, lime juice, and orange liqueur, often served with salt on the rim of the glass.",
+    date: "2022-04-30T18:01:22.785Z",
+  },
+  {
+    name: "Fish and Chips",
+    image: "https://i.imgur.com/XX60O7K.jpg",
+    category: "MainDishes",
+    price: 15.99,
+    time: 25,
+    available: true,
+    description:
+      "A British dish consisting of battered fish and deep-fried potatoes, often served with vinegar and tartar sauce.",
+    date: "2022-04-30T18:01:22.785Z",
+  },
+  {
+    name: "Pad Thai",
+    image: "https://i.imgur.com/2wJHox1.jpg",
+    category: "MainDishes",
+    price: 13.99,
+    time: 25,
+    available: true,
+    description:
+      "Dish made with stir-fried rice noodles, tofu, shrimp, egg, and bean sprouts, fish sauce, and palm sugar.",
+    date: "2022-04-30T18:01:22.785Z",
+  },
+  {
+    name: "Hot Dog",
+    image: "https://i.imgur.com/b93p0Tb.jpg",
+    category: "Appetizers",
+    price: 5.99,
+    time: 10,
+    available: true,
+    description:
+      "A type of sausage served in a sliced bun and typically garnished with ketchup, mustard, and onions.",
+    date: "2022-04-30T18:01:22.785Z",
+  },
+];
 
-module.exports = productsToBulk
+module.exports = productsToBulk;
