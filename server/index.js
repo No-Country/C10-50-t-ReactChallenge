483be236ca14bec9bb927ab3a8766857fd4b9629
Gasnis <gastonsaravia112@gmail.com>
2023-04-20 01:21:54 +0200

    e

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://127.0.0.1:5173",
-      "https://footticket.vercel.app/",
+      "https://foodticket.vercel.app/",
     ],
     methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
     credentials: true,
