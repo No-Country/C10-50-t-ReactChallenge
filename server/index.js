ebc33a7fc914232821054316d2fd213ca97ff12d
Gasnis <gastonsaravia112@gmail.com>
2023-04-20 01:12:30 +0200

    build

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173", ""],
     methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
     credentials: true,
   })
